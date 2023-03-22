360232f0937731ff6f8a18f3250dea495d901e62
Justin Trollip <jtrollip@protonmail.com>
2023-03-22 13:39:56 +1000

    Working on Fuji testnet

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ ContributorRole
 * originalTeam
 * coreTeam
 * uxTester
+
+The Deployed NFT on Fuji is:
+* 0xdB18Fe655c9f4F4C528441462dB8deC29247E112
+* https://testnet.snowtrace.io/address/0xdB18Fe655c9f4F4C528441462dB8deC29247E112
+
+NFT Metadata on IPFS
+* QmZJN8pMh3KYpTNkqaixNXZkojVzLPwPctQTg5UXNM1ZCf
+
+NFT Image on IPFS
+* QmUmpEDhR1ZT4vbcTdckjMKWoFSx4gLPW26cNHT1ZGXJuS
--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,3 +1,52 @@
-require("dotenv").config()
-const API_URL = process.env.API_URL
+const Web3 = require('web3');
+require("dotenv").config();
+
+const provider = process.env.API_URL;
+const web3 = new Web3(provider);
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+const abi = require("../artifacts/contracts/NFT.sol/MyNFT.json")
+const contractAddress = process.env.NFT_CONTRACT
+const contract = new web3.eth.Contract(abi.abi, contractAddress)
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest") //get latest nonce
+
+  //the transaction
+  const tx = {
+    from: PUBLIC_KEY,
+    to: contractAddress,
+    nonce: nonce,
+    gas: 500000,
+    data: contract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+  }
+
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
+  signPromise
+    .then((signedTx) => {
+      web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of your transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of your transaction!"
+            )
+          } else {
+            console.log(
+              "Something went wrong when submitting your transaction:",
+              err
+            )
+          }
+        }
+      )
+    })
+    .catch((err) => {
+      console.log("Promise failed:", err)
+    })
+}
+
+mintNFT("ipfs://QmZJN8pMh3KYpTNkqaixNXZkojVzLPwPctQTg5UXNM1ZCf")
 
